fb10a9f96056432889d63535daa76641f0dbb905
Amr <a_m_elsheikh@hotmail.com>
2018-11-02 03:34:21 +0000

    from windows laptop

--- app/src/main/java/com/example/a_m_e/thecalisthenicsblueprint/Fitnessprograms_Disclaimer.java
+++ app/src/main/java/com/example/a_m_e/thecalisthenicsblueprint/Fitnessprograms_Disclaimer.java
@@ -2,6 +2,8 @@ package com.example.a_m_e.thecalisthenicsblueprint;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Fitnessprograms_Disclaimer extends AppCompatActivity {
 
@@ -15,8 +17,14 @@ public class Fitnessprograms_Disclaimer extends AppCompatActivity {
         setContentView(R.layout.fitnessprograms_disclaimer);
         mDisclaimerAdapter = new DisclaimerAdapter(getSupportFragmentManager());
 
-
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_disclaimer);
+        toolbar.setNavigationIcon(R.drawable.ic_backspace);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
 }
