0d959b11a23c9fc8c15846080b50c3bdc2f9eac3
Amr <a_m_elsheikh@hotmail.com>
2018-11-05 00:42:27 +0000

    from windows laptop

--- app/src/main/java/com/example/a_m_e/thecalisthenicsblueprint/Youtube_Flag_Pullups.java
+++ app/src/main/java/com/example/a_m_e/thecalisthenicsblueprint/Youtube_Flag_Pullups.java
@@ -2,17 +2,48 @@ package com.example.a_m_e.thecalisthenicsblueprint;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class Youtube_Flag_Pullups extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class Youtube_Flag_Pullups extends YouTubeBaseActivity {
+
+    YouTubePlayerView youTubePlayerView;
+    Button button;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.youtube_flag_pullups);
 
+        button = findViewById(R.id.button_play_flagpullups);
+        youTubePlayerView = findViewById(R.id.flagpullups_youtubePlay);
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+                youTubePlayer.loadVideo("nEQQle9-0NA");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize(YoutubeConfig.getApiKey(), onInitializedListener);
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
